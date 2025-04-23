1447bf093536510f01c457bba5e2b8c2903b0cf4
brycehazen <39276420+brycehazen@users.noreply.github.com>
2025-04-23 16:58:06 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ from setuptools import setup
 
 setup(
     name="keyring_cli",
-    version="1.0",
-    py_modules=["keyring_cli"],
+    version="1.1",  # Incremented version for new functionality
+    py_modules=["keyring_cli", "secure_keyring"],  # Added secure_keyring module
     install_requires=[
         "keyring",
-        "cryptography",
+        "cryptography", 
         "pywin32",
         "toml",  
     ],
     entry_points={
         "console_scripts": [
-            "keyring-cli=keyring_cli:main",
+            "keyring=keyring_cli:main",
         ],
     },
 )
